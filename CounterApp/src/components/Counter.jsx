887a5cf2ad756072b50d870b3716057d44076da3
AnkurKumar160305 <ankurthemewtwo@gmail.com>
2025-09-04 22:30:26 +0530

    finished the project of Counter App

---
+++ CounterApp/src/components/Counter.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from 'react'
+import "./Counter.css";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  let handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  let handleSub = () => {
+    setCount(count - 1);
+  };
+
+  let handleReset = () => {
+    setCount(0);
+  };
+  return (
+    <div className="counter">
+      <h1>Counter App</h1>
+      <div className="main">
+        <div className="top">
+          <button class="sub" onClick={handleSub}>
+            -
+          </button>
+          <p class="text">{count}</p>
+          <button class="add" onClick={handleAdd}>
+            +
+          </button>
+        </div>
+        <button className="bottom" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
+
+//rafce
