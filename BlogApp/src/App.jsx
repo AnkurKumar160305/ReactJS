0f825e3c968ba88b4f4e96d31d20041f59e47fa8
AnkurKumar160305 <ankurthemewtwo@gmail.com>
2025-09-11 23:25:47 +0530

    added api

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+  const [arr,setArr]=useState([])
+
+  const main=async()=>{
+    const res=await fetch("https://newsapi.org/v2/everything?q=tesla&from=2025-08-11&sortBy=publishedAt&apiKey=e71d935f77de45dc953bf2f66f617920")
+    const data=await res.json();
+    setArr(data.articles)
+    console.log(data.articles);
+  }
+
+  useEffect(()=>{
+    main();
+  },[])
+
   return (
     <>
-      
     </>
   )
 }
