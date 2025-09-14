88b4c1b213db958ec82b3d1ac75ddf2f150e1d62
AnkurKumar160305 <ankurthemewtwo@gmail.com>
2025-09-14 23:21:28 +0530

    adited files in Blog app

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
-import Blog from "./components/Blog";
+import {ReactRouter, Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+import Blogs from "./components/Blogs";
+import BlogDetails from "./components/BlogDetails";
+import AddBlog from "./components/AddBlog";
 import "./App.css";
 
 function App() {
@@ -7,13 +11,6 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   const main = async () => {
-    setLoading(true);
-    const res = await fetch(
-      "https://newsapi.org/v2/everything?q=apple&from=2025-09-12&to=2025-09-12&sortBy=popularity&apiKey=e71d935f77de45dc953bf2f66f617920"
-    );
-    const data = await res.json();
-    setArr(data.articles || []);
-    setLoading(false);
   };
 
   useEffect(() => {
@@ -28,18 +25,6 @@ function App() {
     <>
       <h1 className="main-heading">News App</h1>
       <div className="news-container">
-        {loading ? (
-          <p>Loading articles...</p>
-        ) : (
-          arr.map((item, index) => (
-            <Blog
-              key={index}
-              title={item.title}
-              image={item.urlToImage}
-              content={item.content}
-            />
-          ))
-        )}
       </div>
     </>
   );
